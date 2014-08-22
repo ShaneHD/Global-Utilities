bf106643eb0ac7a89581fdaa023b1f456df0a872
Shane <Shane@Shane-PC.home>
2014-08-22 20:04:36 +0000

    Created an android utilities project

---
+++ Global Utilities Android/src/ga/shane/utilities/android/BluetoothHelper.java
@@ -0,0 +1,83 @@
+package ga.shane.utilities.android;
+
+import java.util.Set;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+/** @author http://www.shane.ga */
+public class BluetoothHelper extends BroadcastReceiver {
+	private final BluetoothAdapter btAdapter;
+	private final Set<BluetoothDevice> devices;
+	
+	public BluetoothHelper() {
+		Activity main = AndroidUtils.getMainActivity();
+		btAdapter = BluetoothAdapter.getDefaultAdapter();
+		
+		if(!isBluetoothSupported()) {
+			AndroidUtils.toast("Bluetooth isn't supported!");
+			main.finish();
+		}
+		
+		main.registerReceiver(this, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+		main.registerReceiver(this, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED));
+		main.registerReceiver(this, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
+		main.registerReceiver(this, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+		
+		if(!btAdapter.isEnabled())
+			main.startActivityForResult(new Intent(btAdapter.ACTION_REQUEST_ENABLE), 1);
+			
+		devices = btAdapter.getBondedDevices();
+	}
+	
+	/**
+	 * Is the {@link Set} of paired devices empty<br>
+	 * Returns true when no devices are paired
+	 */
+	public boolean isEmpty() {
+		return devices.size() == 0;
+	}
+	
+	/**
+	 * @return A {@link Set} containing all paired devices
+	 */
+	public Set<BluetoothDevice> getPairedDevices() {
+		return devices;
+	}
+	
+	/**
+	 * Call this in the main activity's onPause() method<br>
+	 * TODO look into a listener for this
+	 */
+	public void onPause() {
+		AndroidUtils.getMainActivity().unregisterReceiver(this);
+	}
+	
+	/**
+	 * Call this in the main activity's onActivityResult(int, int, Intent) method<br>
+	 * TODO look into a listener for this
+	 */
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(resultCode == Activity.RESULT_CANCELED) {
+			AndroidUtils.toast("Bluetooth must be enabled to use this feature.");
+			AndroidUtils.getMainActivity().finish();
+		}	
+	}
+	
+	/**
+	 * @return Whether bluetooth is supported or not
+	 */
+	public boolean isBluetoothSupported() {
+		return btAdapter != null;
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+	}
+}
