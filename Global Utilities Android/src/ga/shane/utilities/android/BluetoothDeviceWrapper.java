370226c65276e4486e67134831c5333f5c13ce18
Shane <Shane@Shane-PC.home>
2014-08-23 17:28:21 +0000

    Preppd for in/out stream stuff

--- Global Utilities Android/src/ga/shane/utilities/android/BluetoothDeviceWrapper.java
+++ Global Utilities Android/src/ga/shane/utilities/android/BluetoothDeviceWrapper.java
@@ -1,6 +1,12 @@
 package ga.shane.utilities.android;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
 
 /**
  * Wraps around {@link BluetoothDevice} to add more functionality<br>
@@ -11,9 +17,39 @@ import android.bluetooth.BluetoothDevice;
  */
 public class BluetoothDeviceWrapper implements IBluetoothDevice {
 	private final BluetoothDevice device;
+	private final OutputStream out;
+	private final InputStream in;
 	
 	public BluetoothDeviceWrapper(BluetoothDevice device) {
-		this.device = device;		
+		this.device = device;
+	
+		OutputStream out;
+		InputStream in;
+		
+        try {
+			BluetoothSocket socket = device.createRfcommSocketToServiceRecord(device.getUuids()[0].getUuid());
+			
+			out = socket.getOutputStream();
+			in = socket.getInputStream();
+        } catch (IOException e) {
+			out = null;
+			in = null;
+			e.printStackTrace();
+		}
+        
+        this.out = out;
+        this.in = in;
+	}
+	
+	/**
+	 * Send a {@link String} in byte[] form to the device
+	 */
+	public void send(String text) {
+		try {
+			out.write(text.getBytes());
+		} catch (IOException e) {
+			AndroidUtils.error("Couldn't write String: \"" + text + "\"", e);
+		}
 	}
 	
 	/**
