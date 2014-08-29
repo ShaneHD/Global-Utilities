c91299af93df952f4e6869504b01e63f7b84e311
Shane <Shane@Shane-PC.home>
2014-08-29 06:12:37 +0000

    Added a yes/no option box in WindowsUtils

--- Global Utilities/src/ga/shane/utilities/WindowsUtils.java
+++ Global Utilities/src/ga/shane/utilities/WindowsUtils.java
@@ -18,6 +18,23 @@ public class WindowsUtils {
 	public static void messagebox(String body, String title, int icon, Component... parent) {
 		JOptionPane.showMessageDialog(parent.length > 0 ? parent[0] : null, body, title, icon);
 	}
+
+	/**
+	 * Make a yes/no option box for the user
+	 * @return Was ok pressed?
+	 */
+	public static boolean agreeBox(String body) {
+		JFrame fullscreen = new JFrame();
+		Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+		fullscreen.setSize(size);
+		fullscreen.setAlwaysOnTop(true);
+		
+		int i = JOptionPane.showConfirmDialog(fullscreen, body, "Authorisation required", JOptionPane.YES_NO_CANCEL_OPTION);
+		fullscreen.dispose();
+		fullscreen = null;
+		
+		return i == 0;
+	}
 	
 	/**
 	 * Makes sure that the message is visible no matter what (on any window)
@@ -30,6 +47,22 @@ public class WindowsUtils {
 		fullscreen.setAlwaysOnTop(true);
 		
 		JOptionPane.showMessageDialog(fullscreen, body, title, icon);
+		fullscreen.dispose();
+		fullscreen = null;
+	}
+	
+	/**
+	 * @see {@link #messagebox(String, String, int, Component...)}
+	 */
+	public static void messageBox(String body, String title, int icon, Component... parent) {
+		messagebox(body, title, icon, parent);
+	}
+	
+	/**
+	 * @see {@link #globalMessagebox(String, String, int)}
+	 */
+	public static void globalMessageBox(String body, String title, int icon) {
+		globalMessagebox(body, title, icon);
 	}
 	
 	/**
