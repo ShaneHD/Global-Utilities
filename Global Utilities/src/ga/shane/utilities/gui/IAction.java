4ba707efdea6d7860aba5a5b89e228c232d610a5
Shane <Shane@Shane-PC.home>
2014-08-30 10:48:27 +0000

    doc

--- Global Utilities/src/ga/shane/utilities/gui/IAction.java
+++ Global Utilities/src/ga/shane/utilities/gui/IAction.java
@@ -2,7 +2,15 @@ package ga.shane.utilities.gui;
 
 import java.awt.Component;
 
-/** @author http://www.shane.ga */
+/**
+ * An interface that some of my utilities use<br>
+ * Has one method ({@link #on(Component)})<br><br>
+ * 
+ * EX: In {@link ButtonCreator}, when a button is added, any {@link IAction}s
+ * will run the {@link #on(Component)} method
+ * 
+ * @author http://www.shane.ga
+ */
 public interface IAction {
 	void on(Component c);
 }
