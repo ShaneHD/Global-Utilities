b50827caa443861f82bebc978a1f5f06a3a98ba3
Shane <Shane@Shane-PC.home>
2014-09-01 09:11:32 +0000

    Moved some things from WindowsUtils to PCUtils

---
+++ Global Utilities/src/ga/shane/utilities/PCUtils.java
@@ -0,0 +1,72 @@
+package ga.shane.utilities;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/** @author http://www.shane.ga */
+public class PCUtils {
+	/**
+	 * Show a MessageBox normally
+	 */
+	public static void messagebox(String body, String title, int icon, Component... parent) {
+		JOptionPane.showMessageDialog(parent.length > 0 ? parent[0] : null, body, title, icon);
+	}
+	
+	/**
+	 * Make a yes/no option box for the user
+	 * @return Was ok pressed?
+	 */
+	public static boolean agreeBox(String body) {
+		JFrame fullscreen = new JFrame();
+		Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+		fullscreen.setSize(size);
+		fullscreen.setAlwaysOnTop(true);
+		
+		int i = JOptionPane.showConfirmDialog(fullscreen, body, "Authorisation required", JOptionPane.YES_NO_CANCEL_OPTION);
+		fullscreen.dispose();
+		fullscreen = null;
+		
+		return i == 0;
+	}
+	
+	/**
+	 * NoArg version
+	 * @see {@link #agreeBox(String)}
+	 */
+	public static boolean agreeBox() {
+		return agreeBox("Sure that you want to continue?");
+	}
+	
+	/**
+	 * Makes sure that the message is visible no matter what (on any window)
+	 * @see {@link #messagebox(String, String, int, Component...)}
+	 */
+	public static void globalMessagebox(String body, String title, int icon) {
+		JFrame fullscreen = new JFrame();
+		Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+		fullscreen.setSize(size);
+		fullscreen.setAlwaysOnTop(true);
+		
+		JOptionPane.showMessageDialog(fullscreen, body, title, icon);
+		fullscreen.dispose();
+		fullscreen = null;
+	}
+	
+	/**
+	 * @see {@link #messagebox(String, String, int, Component...)}
+	 */
+	public static void messageBox(String body, String title, int icon, Component... parent) {
+		messagebox(body, title, icon, parent);
+	}
+	
+	/**
+	 * @see {@link #globalMessagebox(String, String, int)}
+	 */
+	public static void globalMessageBox(String body, String title, int icon) {
+		globalMessagebox(body, title, icon);
+	}
+}
