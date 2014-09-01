35c8dd9f298a35145b885f43ef69f4cee40781f4
Shane <Shane@Shane-PC.home>
2014-09-01 13:25:22 +0000

    Keylistener is now separate

--- Global Utilities/src/ga/shane/utilities/gui/UrlTextFieldButton.java
+++ Global Utilities/src/ga/shane/utilities/gui/UrlTextFieldButton.java
@@ -1,11 +1,5 @@
 package ga.shane.utilities.gui;
 
-import java.awt.Component;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
-import javax.swing.Action;
-import javax.swing.Icon;
 
 /**
  * A {@link BJButton} designed to be used with a {@link UrlTextField}<br>
@@ -13,28 +7,9 @@ import javax.swing.Icon;
  * 
  * @author http://www.shane.ga
  */
-public class UrlTextFieldButton extends BJButton implements KeyListener {	
+public class UrlTextFieldButton extends BJButton {	
 	public UrlTextFieldButton(UrlTextField url, String text) {
 		super(text);
-		url.addKeyListener(this);
-	}
-	
-	public void update(Component c) {
-		setEnabled(((UrlTextField) c).valid());
-	}
-	
-	@Override
-	public void keyPressed(KeyEvent e) {
-		update(e.getComponent());
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		update(e.getComponent());
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		update(e.getComponent());
+		url.addKeyListener(new UrlTextFieldKeyListener(this));
 	}
 }
