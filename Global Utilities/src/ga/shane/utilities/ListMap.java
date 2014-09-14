feb56ef64107d68ea4529656dcd23e5c0ed7e9f0
Shane <Shane@Shane-PC>
2014-09-14 03:12:37 +0100

    Added put method

--- Global Utilities/src/ga/shane/utilities/ListMap.java
+++ Global Utilities/src/ga/shane/utilities/ListMap.java
@@ -27,4 +27,26 @@ public class ListMap<K, V> extends HashMap<K, ArrayList<V>> {
 	public ListMap(Map<?extends K, ?extends ArrayList<V>> m) {
 		super(m);
 	}
+	
+	/**
+	 * @see {@link #put(K, V)}
+	 */
+	public ArrayList<V> put(K key, ArrayList<V> value) {
+		throw new RuntimeException("Don't use this");
+	}
+	
+	/**
+	 * <b><font color='green'>USE THIS</font></b>
+	 */
+	@SuppressWarnings("rawtypes")
+	public ListMap put(K key, V value) {
+		if(containsKey(key))
+			get(key).add(value);
+		else {
+			put(key, new ArrayList<V>());
+			put(key, value);
+		}
+		
+		return this;
+	}
 }
