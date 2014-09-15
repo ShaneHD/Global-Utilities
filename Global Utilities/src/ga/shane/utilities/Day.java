9c6a5ea21713eca9c026390f18c9834ec1b80651
Shane <Shane@Shane-PC>
2014-09-15 20:13:28 +0100

    Added Day enum

---
+++ Global Utilities/src/ga/shane/utilities/Day.java
@@ -0,0 +1,29 @@
+package ga.shane.utilities;
+
+/**
+ * Enum containing all days of the week<br>
+ * in short form(E.G. MON), and their<br>
+ * long form(E.G. Monday)
+ * @see {@link #full_name}
+ * 
+ * @author http://www.shane.ga
+ */
+public enum Day {
+	MON("Monday"),
+	TUE("Tuesday"),
+	WED("Wednesday"),
+	THU("Thursday"), 
+	FRI("Friday"),
+	SAT("Saturday"),
+	SUN("Sunday");
+	
+	/**
+	 * The day of the week in full form<br>
+	 * E.G. MON becomes Monday
+	 */
+	public final String full_name;
+	
+	Day(String full) {
+		full_name = full;
+	}
+}
