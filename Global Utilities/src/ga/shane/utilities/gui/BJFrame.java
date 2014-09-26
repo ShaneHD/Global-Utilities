37d91551f6a38b4d1ca536c109e9bc6f98b3062e
Shane <Shane@Shane-PC>
2014-09-26 16:46:05 +0100

    doc

--- Global Utilities/src/ga/shane/utilities/gui/BJFrame.java
+++ Global Utilities/src/ga/shane/utilities/gui/BJFrame.java
@@ -4,7 +4,13 @@ import javax.swing.JFrame;
 
 /**
  * Stands for BetterJFrame<br>
- * Adds two methods: {@link #init()} & {@link #constructFrame()}
+ * Adds two methods: {@link #init()} & {@link #constructFrame()}<br><br>
+ *
+ * <b style='color:red;'>
+	 * =========================================================<br>
+	 * Both methods are called AUTOMATICALLY in the constructors<br>
+	 * =========================================================
+ * </b>
  * 
  * @see {@link JFrame}
  * @author http://www.shane.ga
@@ -23,13 +29,17 @@ public abstract class BJFrame extends JFrame {
 	/**
 	 * Anything that you'd normally have in<br>
 	 * the constructor (excluding {@link JFrame} stuff)<br><br>
-	 * Goes BEFORE {@link #constructFrame()}
+	 * This is called BEFORE {@link #constructFrame()}
+	 * 
+	 * @see {@link #constructFrame()}
 	 */
 	protected abstract void init();
 	
 	/**
 	 * Place anything {@link JFrame} related in here<br>
-	 * This is called AFTER {@link #init()}
+	 * This is called AFTER {@link #init()}<br><br>
+	 * E.G. stuff like {@link #setDefaultCloseOperation(int)}, {@link #setResizable(boolean)}<br>
+	 * etc go here.
 	 */
 	protected abstract void constructFrame();
 }
