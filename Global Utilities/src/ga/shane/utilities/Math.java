de195305d2b5507112e264160f159c9eda4227ce
Shane <Shane@Shane-PC>
2014-12-04 14:19:24 +0000

    Math

---
+++ Global Utilities/src/ga/shane/utilities/Math.java
@@ -0,0 +1,11 @@
+package ga.shane.utilities;
+
+public class Math {
+	public static int center(int width, int maxwidth) {
+		return maxwidth / 2 - (width / 2);
+	}
+	
+	public static float center(float width, float maxwidth) {
+		return maxwidth / 2 - (width / 2);
+	}
+}
