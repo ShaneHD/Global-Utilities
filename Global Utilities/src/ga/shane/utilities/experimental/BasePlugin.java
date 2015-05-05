cbd419b00e736d99b8cffa60d93807aab5603520
Shane <Shane@Shane-PC>
2015-05-06 00:56:52 +0100

    Plugin information now annotation based

--- Global Utilities/src/ga/shane/utilities/experimental/BasePlugin.java
+++ Global Utilities/src/ga/shane/utilities/experimental/BasePlugin.java
@@ -365,26 +365,11 @@ Exhibit B - "Incompatible With Secondary Licenses" Notice
 package ga.shane.utilities.experimental;
 
 /** 
- * Base class for an external plugin
+ * Base class for an external plugin<br>
+ * <b style='color:red;'>Annotate class with {@link PluginInfo}</b>
  * 
  * @author http://www.shane.ga
  */
-public abstract class BasePlugin<T> {
-	public abstract void load(T arg);
-	
-	/**
-	 * The version of this plugin (will be used for updating, and possibly on a plugin repo)
-	 */
-	/*public double version() {
-		//return 1.0;
-	}*/
-	public abstract double version();
-	
-	/**
-	 * The author of this plugin
-	 */
-	/*public String author() {
-		return "http://www.shane.ga";
-	}*/
-	public abstract String author();
+public interface BasePlugin<T> {
+	void load(T arg);
 }
