8d20e6209c9a2ee7a5f05335b336eed56db7d182
Shane <Shane@Shane-PC>
2016-07-17 21:33:39 +0100

    l

--- Global Utilities/src/com/github/shanehd/utilities/LogHandler.java
+++ Global Utilities/src/com/github/shanehd/utilities/LogHandler.java
@@ -1,5 +1,7 @@
 package com.github.shanehd.utilities;
 
+import javax.swing.*;
+
 /**
  * @author https://www.github.com/ShaneHD
  *         Created by Shane on 07/07/2016.
@@ -7,6 +9,8 @@ package com.github.shanehd.utilities;
 public abstract class LogHandler implements ILog {
     public abstract void print(String line);
     public abstract String format(String type, String line);
+
+    //TODO enum with different levels of logging gtfo with this shit
 
     @Override
     public void info(String s) {
@@ -39,5 +43,18 @@ public abstract class LogHandler implements ILog {
     public void fatalError(Throwable t) {
         error("Fatal error: " + t);
         t.printStackTrace();
+    }
+
+    public final int getJOptionPaneType(String type) {
+        switch(type) {
+            case "INFO":
+                return JOptionPane.INFORMATION_MESSAGE;
+            case "WARNING":
+                return JOptionPane.WARNING_MESSAGE;
+            case "ERROR":
+                return JOptionPane.ERROR_MESSAGE;
+        }
+
+        return 0;
     }
 }
