7ae7251abdd170a3a3b28b1643b01630e6df076e
Shane <Shane@Shane-PC>
2016-10-03 17:23:49 +0100

    jfx alerts

--- Global Utilities/src/com/github/shanehd/utilities/gui/javafx/JFXUtils.java
+++ Global Utilities/src/com/github/shanehd/utilities/gui/javafx/JFXUtils.java
@@ -1,6 +1,9 @@
 package com.github.shanehd.utilities.gui.javafx;
 
+import javafx.scene.control.Alert;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import javafx.scene.web.WebView;
 
 /**
  * JavaFX Utils
@@ -15,5 +18,48 @@ public class JFXUtils {
     public static double calcDisplayWidth(Object property) {
         Text text = new Text(property.toString());
         return text.getLayoutBounds().getWidth();
+    }
+
+    /**
+     * Quick way of showing an {@link Alert} that supports HTML formatting in its <code>content</code><br/>
+     * Calls {@link #alert(Alert.AlertType, String, String, double, double, boolean)} with <code>380</code>
+     *      width, and <code>256</code> height.
+     *
+     * @param type The type of alert to show ({@link javafx.scene.control.Alert.AlertType})
+     * @param header Text shown above the content
+     * @param content The text to display
+     * @param wait Should the program wait for this alert to be closed? (should {@link Alert#showAndWait()}
+     *             be called instead of {@link Alert#show()}?)
+     */
+    public static void alert(Alert.AlertType type, String header, String content, boolean wait) {
+        alert(type, header, content, 380, 256, wait);
+    }
+
+    /**
+     * Quick way of showing an {@link Alert} that supports HTML formatting in its <code>content</code>
+     *
+     * @param type The type of alert to show ({@link javafx.scene.control.Alert.AlertType})
+     * @param header Text shown above the content
+     * @param content The text to display
+     * @param prefWidth Preferred width of the dialog
+     * @param prefHeight Preferred height of the dialog
+     * @param wait Should the program wait for this alert to be closed? (should {@link Alert#showAndWait()}
+     *             be called instead of {@link Alert#show()}?)
+     */
+    public static void alert(Alert.AlertType type, String header, String content, double prefWidth, double prefHeight, boolean wait) {
+        Alert alert = new Alert(type);
+        alert.setHeaderText(header);
+
+        Pane pane = alert.getDialogPane();
+        pane.setPrefSize(prefWidth, prefHeight);
+
+        WebView html = new WebView();
+        html.getEngine().loadContent(content);
+        alert.getDialogPane().setContent(html);
+
+        if(wait)
+            alert.showAndWait();
+        else
+            alert.show();
     }
 }
