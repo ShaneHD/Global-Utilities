e2993771145e389dd043191e6ef4f3a3b26d4f68
Shane <Shane@Shane-PC>
2016-10-04 14:01:18 +0100

    better debug

--- Global Utilities/src/com/github/shanehd/utilities/net/PacketHandler.java
+++ Global Utilities/src/com/github/shanehd/utilities/net/PacketHandler.java
@@ -6,46 +6,63 @@ import java.util.logging.Logger;
 import com.github.shanehd.utilities.StringUtils;
 
 /**
+ * Processes incoming {@link Command}s<br/>
+ * Simply place [label, command instance] into {@link #commands} or {@link #noargCommands},
+ * 		and call either {@link #handle(String)} or {@link #handle(String, String[])} depending
+ * 		on whether arguments are present or not
+ *
  * @author https://www.github.com/ShaneHD
  */
 public class PacketHandler {
 	private final NetworkHelper out;
 	protected final Logger log;
+	/** A {@link java.util.Map} of all added {@link Command}s */
 	public final HashMap<String, Command> commands = new HashMap<String, Command>();
+	/** A {@link java.util.Map} of all added {@link com.github.shanehd.utilities.net.Command.NoArgs} */
 	public final HashMap<String, Command.NoArgs> noargCommands = new HashMap<String, Command.NoArgs>();
 
 	public PacketHandler(NetworkHelper out, Logger log) {
 		this.out = out;
 		this.log = log;
 	}
-	
-	/** 
-	 * Handle input
+
+	private void error(String command, int argsSize, Throwable t) {
+		t.printStackTrace();
+		log.warning("Command " + StringUtils.quote(command) + " failed with args size " + StringUtils.quote(argsSize) + "; INFO= " + StringUtils.format(t));
+	}
+
+	/**
+	 * @return The requested command
+	 * @throws IllegalArgumentException If the command isn't added
+     */
+	private Object safeget(String command, HashMap commands) throws IllegalArgumentException {
+		if(!commands.containsKey(command))
+			throw new IllegalArgumentException("Command " + StringUtils.quote(command) + " doesn't exist!");
+
+		return commands.get(command);
+	}
+
+	/**
+	 * Handle an incoming {@link Command} with no arguments
 	 */
-	public void handle(String s) {
+	@SuppressWarnings("unchecked")
+	public final void handle(String command) {
 		try {
-			noargCommands.get(s.toLowerCase()).handle(out, s);
+			((Command.NoArgs) safeget(command, noargCommands)).handle(out, command);
 		} catch(Exception e) {
-			e.printStackTrace();
-			log.warning("Command " + StringUtils.quote(s) + " failed. No args.");
+			error(command, 0, e);
 		}
 	}
 	
 	/**
-	 * Handle input with arguments
-	 * @see #handle(String)
+	 * Handle an incoming {@link Command} with arguments
 	 */
-	public void handle(String s, String[] args) {		
+	@SuppressWarnings("unchecked")
+	public final void handle(String command, String[] args) {
 		try {
-			commands.get(s.toLowerCase()).handle(out, s, args[1], args); 
+			((Command) safeget(command, commands)).handle(out, command, args[1], args);
 		} catch(Exception e) {
-			e.printStackTrace();
-			log.warning("Command " + StringUtils.quote(s) + " failed.");
-			
-			for(String s1 : args)
-				log.warning("[ARG]: " + s1);
-			
-			log.warning("[end of args]");
+			error(command, args.length, e);
 		}
 	}
 }
