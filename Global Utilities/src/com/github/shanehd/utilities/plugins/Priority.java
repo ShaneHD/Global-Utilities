0946b5d9cbb2c472503ce8bac6647932f0b47518
Shane <Shane@Shane-PC.(none)>
2016-12-07 23:45:07 +0000

    no kotlin

---
+++ Global Utilities/src/com/github/shanehd/utilities/plugins/Priority.java
@@ -0,0 +1,9 @@
+package com.github.shanehd.utilities.plugins;
+
+/**
+ * @author https://www.github.com/ShaneHD
+ *         Created by Shane on 07/12/2016.
+ */
+public enum Priority {
+    LOW, NORMAL, HIGH
+}
--- Global Utilities/src/com/github/shanehd/utilities/plugins/Priority.kt
+++
@@ -1,5 +0,0 @@
-package com.github.shanehd.utilities.plugins
-
-enum class Priority {
-	LOW, NORMAL, HIGH
-}
